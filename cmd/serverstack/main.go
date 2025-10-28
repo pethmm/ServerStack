d50e9c5555df82a30ec2f12b140752e5c04eb3bd
Pety <38808074+pethmm@users.noreply.github.com>
2025-10-28 21:20:02 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/serverstack/main.go
@@ -0,0 +1,23 @@
+// cmd/serverstack/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "serverstack/internal/serverstack"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := serverstack.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
